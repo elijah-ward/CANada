b7c907d2f22ff0989245671f6f38915c4810678f
Elijah Ward <elijahward.dev@gmail.com>
2018-11-18 22:58:35 -0500

    filter brakes messages based on arbitration id

--- critical_ecu/brakes.py
+++ critical_ecu/brakes.py
@@ -3,17 +3,17 @@ import can
 class Brakes:
 
 	def __init__(self):
-		self.gas = 0.0
+		self.pressure = 0.0
+		self.filters = [{
+			"can_id": 0x79,
+			"can_mask": 0x21
+		}]
 
 	def listen(self):
 		bus = can.Bus(interface='virtual',
-    		channel='vcan0',
-    		bitrate=50000)
+    		channel='vcan0')
 
-		# iterate over received messages
-		for msg in bus:
-		    print("BRAKES - {}: {}".format(msg.arbitration_id, msg.data))
-
+		bus.set_filters(self.filters)
 
 		# or use an asynchronous notifier
 		notifier = can.Notifier(bus, [can.Logger("recorded.log"), can.Printer()])
