57df3ec3aba042d65db3fb814afc9b9c53c13692
Elijah Ward <elijahward.dev@gmail.com>
2018-11-18 23:24:13 -0500

    message filtering for fuel injection

--- critical_ecu/fuel_injection.py
+++ critical_ecu/fuel_injection.py
@@ -1,17 +1,21 @@
 import can
+from utils.mask_helper import MaskHelper
 
 class FuelInjection:
 
 	def __init__(self):
 		self.gas = 0.0
+		self.filters = [{
+			"can_id": 0xA0,
+			"can_mask": 0x21
+		}]
 
 	def listen(self):
 		bus = can.Bus(interface='virtual',
     		channel='vcan0')
 
-		# iterate over received messages
-		for msg in bus:
-		    print("FUEL INJECTION - {}: {}".format(msg.arbitration_id, msg.data))
+		mh = MaskHelper()
+		mh.check_match(100, self.filters[0]["can_id"], self.filters[0]["can_mask"])
 
 		# or use an asynchronous notifier
 		notifier = can.Notifier(bus, [can.Logger("recorded.log"), can.Printer()])
