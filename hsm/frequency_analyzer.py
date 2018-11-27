93d20dd68a7c4de3667df42f8d470624b201cf75
Elijah Ward <elijahward.dev@gmail.com>
2018-11-26 19:06:11 -0500

    more reasonable HSM behaviour - only display messages of inter-bus comm

--- hsm/frequency_analyzer.py
+++ hsm/frequency_analyzer.py
@@ -35,7 +35,7 @@ class FrequencyAnalyzer(Listener):
 			self.traffic[msg_id]['avg_interval'] = self.traffic[msg_id]['cumulative_interval'] / self.traffic[msg_id]['count']
 			self.traffic[msg_id]['last_timestamp'] = current_timestamp
 
-		print('Detecting: Adjusting normal range for message with id of {}. Training time remaining: {}s.'.format(msg_id, int(self.training_end - time.time())))
+		print(colored('Detecting: Adjusting normal range for message with id of {}. Training time remaining: {}s.'.format(msg_id, int(self.training_end - time.time())), 'yellow'))
 
 	def enforce_range(self, msg):
 
@@ -54,16 +54,18 @@ class FrequencyAnalyzer(Listener):
 			if current_interval < threshold:
 				print(colored('\nIRREGULAR MESSAGE FREQUENCY - BLOCKING MESSAGE\navg_interval - {}, current_interval - {}, last_timestamp: {}\n'
 					.format(threshold, current_interval, prior_timestamp), 'red'), colored('{}\n'.format(msg), 'red'))
-			elif Config.components[Config.id_assignment[msg_id]]['can_channel'] != self.channel:
+			else:
 				print(colored('Received Inter-Bus Message... Relaying:\navg_interval - {}, current_interval - {}, last_timestamp: {}\n'
 					.format(threshold, current_interval, prior_timestamp), 'green'), colored(msg, 'green'))
 				self.outbound_bus.send(msg)
 
 	def on_message_received(self, msg):
 
+		msg_id = msg.arbitration_id
+
 		if time.time() < self.training_end:
 			self.detect_normal_range(msg)
-		else:
+		elif Config.components[Config.id_assignment[msg_id]]['can_channel'] != self.channel:
 			self.enforce_range(msg)
 
 
