686e737954c487d2b21e65ef876f38812d5e07d3
Elijah Ward <elijahward.dev@gmail.com>
2018-12-07 12:27:39 -0500

    noop hsm module for simple inter-bus relay

---
+++ hsm/noop.py
@@ -0,0 +1,29 @@
+import time
+import can
+from termcolor import colored
+from can.listener import Listener
+from config import Config
+
+class Noop(Listener):
+
+	def __init__(self, channel):
+		self.channel = channel
+
+		# Outward bus, target channel is vcan0 if this listener is receiving on vcan1 and vice-versa
+		self.outbound_channel = 'vcan1' if channel == 'vcan0' else 'vcan0'
+		self.outbound_bus = can.Bus(interface='virtual', channel=self.outbound_channel)
+
+	def relay(self, msg):
+
+		print(colored('\nRECEIVED INTER-BUS MESSAGE... Relaying:\n','green'), colored(msg, 'green'))
+		self.outbound_bus.send(msg)
+
+	def on_message_received(self, msg):
+
+		msg_id = msg.arbitration_id
+
+		if Config.components[Config.id_assignment[msg_id]]['can_channel'] != self.channel:
+			self.relay(msg)
+
+
+
