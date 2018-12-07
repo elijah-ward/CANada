c2379bf9cba92d31723ba1273aae227a12e424b4
Elijah Ward <elijahward.dev@gmail.com>
2018-12-07 12:26:35 -0500

    compromised ecu with intrusion monitoring

---
+++ adversaries/intrusion_infotainment.py
@@ -0,0 +1,30 @@
+import can
+import time
+from config import Config
+from messages.message_factory import Message
+from hsm.intrusion_detection import ECUMonitor
+
+'''
+The following class represents an example where an adversary has gained control over the infotainment unit
+and uses it to flood messages onto the bus
+'''
+
+class IntrusionInfotainment:
+
+	def __init__(self):
+		self.is_malicious = False
+		self.bus = can.Bus(interface='virtual', channel='vcan1')
+		self.monitor_hsm = ECUMonitor()
+
+	def start(self):
+		start_time = time.time()
+
+		while True:
+			self.monitor_hsm.scan(self.is_malicious, self.bus)	
+			current_time = time.time()
+			if ( current_time - start_time ) >= Config.intrusion_detection['start_delay']:
+				self.is_malicious = True
+
+			time.sleep(Config.intrusion_detection['scan_delay'])
+
+
---
+++ hsm/intrusion_detection.py
@@ -0,0 +1,26 @@
+import time
+import random
+import can
+from termcolor import colored
+from config import Config
+
+class ECUMonitor:
+
+	def __init__(self):
+		self.detection_rate = Config.intrusion_detection['detection_rate']
+
+	def scan(self, malicious_state, bus):
+		threat_detected = False
+		print(colored('ECU monitor scanning for threats...', 'yellow'))
+		if malicious_state:
+			detection_roll = random.random()
+			if detection_roll <= self.detection_rate:
+				threat_detected = True
+				print(colored('INTRUSION DETECTED, ISOLATING NON-CRITICAL ECU', 'red'))
+				bus.shutdown()
+		if not threat_detected:
+			print(colored('No threats detected... hibernating.', 'yellow'))
+
+
+
+
